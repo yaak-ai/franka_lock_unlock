0cc05489544c8bb3565b61af05f779b84debe967
mario <mariocorsanici>
2023-11-21 17:14:48 +0100

    added shutdown and fr3 lock and unlock

---
+++ constant.py
@@ -0,0 +1,10 @@
+#for fr3
+unlock = "unlock"
+lock = "lock"
+shoutdown = "/admin/api/shutdown"
+deskApiJoints = "/desk/api/joints/"
+# # for panda
+# unlock = "open-brakes"
+# lock = "clock-brakes"
+# shoutdown = "/admin/api/shutdown"
+# deskApiJoints = "/desk/api/robot/"
